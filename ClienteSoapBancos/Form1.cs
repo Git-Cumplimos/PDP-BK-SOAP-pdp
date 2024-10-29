6be29fc9b3131d0edb5beb57c868c177ea4f313f
LisbethNunez13 <114171377+LisbethNunez13@users.noreply.github.com>
2024-10-29 09:37:27 -0500

    Add files via upload

---
+++ ClienteSoapBancos/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteSoapBancos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            servicioDavivienda.ServidorSoapDaviviendaClient oClient = new servicioDavivienda.ServidorSoapDaviviendaClient();
+            string res = oClient.consultaRecaudo(5);
+            MessageBox.Show(res);
+        
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
